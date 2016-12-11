2753651274114f34cef68fb4ecae19b2c95d7775
Sthefany Floriano <Sthefany Floriano>
2016-12-10 21:49:57 -0500

    upload project structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require("gulp"),
 	concat = require("gulp-concat"),
-	uglify = require("gulp-uglify");
+	uglify = require("gulp-uglify"),
+	sass = require("gulp-sass"),
+	cleancss = require("gulp-clean-css");
 
 gulp.task("devjs", function () {
 	gulp.src("src/js/*.js")
 	.pipe(concat("main.js"))
 	.pipe(uglify())
 	.pipe(gulp.dest("dist/js/"))
+});
+
+gulp.task("devcss", function(){
+	gulp.src(["src/css/*.css", "src/sass/*.scss"])
+	.pipe(sass())
+	.pipe(concat("style.min.css"))
+	.pipe(cleancss({keepSpecialComments : 1}))
+	.pipe(gulp.dest("dist/css/"))
 });
