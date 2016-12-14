8d8a8e06cda01c3f305ba2360bd3e5cb7025e6a1
Andrea Maucaylli <andreamaucaylli@gmail.com>
2016-12-14 12:07:06 -0500

    endpoint changes

--- app.js
+++ app.js
@@ -1,7 +1,67 @@
 var express = require("express");
 var app = express();
+var dream = require('dreamjs');
+
+var distritos = ['Lima', 'Ate', 'Barranco', 'Breña', 'Comas', 'Chorrillos', 'El Agustino', 'Jesús María', 
+  'La Molina', 'La Victoria', 'Lince', 'Magdalena del Mar', 'Miraflores', 'Pueblo Libre', 'Puente Piedra',
+  'Rimac', 'San Isidro', 'Independencia', 'San Juan de Miraflores', 'San Luis', 'San Martin de Porres', 'San Miguel',
+  'Santiago de Surco', 'Surquillo', 'Villa María del Triunfo', 'San Juan de Lurigancho', 'Santa Rosa', 'Los Olivos',
+  'San Borja', 'Villa El Savador', 'Santa Anita', 'Callao', 'Bellavista', 'Carmen de la Legua', 'La Perla', 'La Punta', 'Ventanilla'];
+var precios = [46, 52, 93, 67, 121, 211];
+var evaluaciones = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var tipo = ["Departamento", "Casa", "Habitación"];
+var titulo = ["Habitación con vista al mar", "Habitación entrada independiente"];
+
+dream.customType('pi', function () {
+	return Math.PI;
+});
+dream.customType('distrito', function (helper) {
+	return helper.oneOf(distritos);
+});
+dream.customType('precio', function (helper) {
+	return helper.oneOf(precios);
+})
+dream.customType('evaluaciones', function (helper) {
+	return helper.oneOf(evaluaciones);
+})
+dream.customType('tipo', function (helper) {
+	return helper.oneOf(tipo);
+})
+dream.customType('titulo', function (helper) {
+	return helper.oneOf(titulo);
+})
+
+
+var lugares = dream
+  .schema({
+  	titulo: 'titulo',
+    distrito: 'distrito',
+    precio: 'precio',
+    evaluaciones: 'evaluaciones',
+    tipo: 'tipo',
+    foo: function () {
+      return 'bar';
+    },
+    pi: 'pi',
+    hello: 'hello'
+  })
+  .generateRnd(100)
+  .output();
+  
+app.get('/lugares', function (req, res) {
+    var lugar = req.query.lugar;
+    var lugaresFiltrados = [];
+    for (var i = 0; i < lugares.length; i++) {
+        var lugar = lugares[i];
+        if (lugar.distrito == lugar) {
+            lugaresFiltrados.push(lugar);
+        }       
+    }
+    res.send(lugaresFiltrados);
+})
+
 app.use(express.static(__dirname + "/src"));
 app.use(express.static(__dirname + "/static"));
-app.listen(8080 , function(){
+app.listen(3000 , function(){
   console.log("Servidor Encendido");
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,93 @@
-var lugarIngresado = localStorage.getItem("");
-var llegadaIngresada = localStorage.getItem("");
-var salidaIngresada = localStorage.getItem("");
-var numHuespedes = localStorage.getItem("");
+var template =  "<div class='col s12 m12 l6'>" +
+                    "<div class='card'>" +
+                        "<div class='card-image'>" +
+                            "<img src='/assets/img/img-2.jpg'>" +
+                            "<span class='card-title precio'>s/. {{precio}}</span>" +                               
+                        "</div>" +
+                        "<div class='col s2'>" +
+                            "<img src='/assets/img/user-1.jpg' alt='' class='circle responsive-img image-user'>" +
+                        "</div>" +
+                        "<div class='card-content'>" +
+                           	"<p><a class='titulo' href='#''>{{titulo}}</a></p>" +
+                           	"<ul>" +
+                               	"<li>{{tipo}}</li>" +
+                               	"<li>* {{num_huespedes}} huéspedes</li>" +
+                               	"<li>* {{num_evaluaciones}} evaluaciones</li>" +
+                           	"</ul>" +
+                        "</div>" +
+                    "</div>" +
+                "</div>";
+
+var lugarIngresado = localStorage.getItem("lugar");
+var llegadaIngresada = localStorage.getItem("llegada");
+var salidaIngresada = localStorage.getItem("salida");
+var numHuespedes = localStorage.getItem("huespedes");
 
 var inputLugar = $('#lugar');
 var inputLlegada = $('#llegada');
 var inputSalida = $('#salida');
+var inputHuespedes = $('#huespedes');
 var btnBuscar = $('#buscar');
 
 var validarTeclas = function (e) {
 	var ascii = e.keyCode;
-		if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
-			return true;
-		} else {
-			return false;			
-		}
+	if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
+		return true;
+	} else {
+		return false;			
 	}
+}
+
 
 var buscarLugar = function () {
 	var lugar = inputLugar.val();
-	lugarIngresado = localStorage.setItem("lugar", lugar);
-	
+	lugarIngresado = localStorage.setItem("lugar", lugar);	
 	var llegada = inputSalida.val();
 	llegadaIngresada = localStorage.setItem("llegada", llegada);
-
 	var salida = inputLlegada.val();
 	salidaIngresada = localStorage.setItem("salida", salida);
+	var huespedes = inputHuespedes.val();
+	huespedesIngresados = localStorage.setItem("huespedes", huespedes);
 
+    $.when(
+        $.ajax({ 
+          url: "/lugares?lugar="+lugarIngresado,
+          type: "GET"
+        }) 
+    ).then(function(lugares,data){
+        $.each(lugares[0], function (i,lugar) {
+          	var largo = lugares[0].length;
+          	console.log(largo);
+        	initMap();
+        	var lugarResultado = template.replace("{{precio}}", lugar.precio)
+        	.replace("{{titulo}}", lugar.titulo)
+        	.replace("{{tipo}}", lugar.tipo)
+            .replace("{{num_huespedes}}", huespedes)
+            .replace("{{num_evaluaciones}}", lugar.evaluaciones)
+            $("#resultados").append(lugarResultado);
+        });
+	});	
+}
+
+var validarInputs = function () {
 	if (inputLugar != '' && inputLlegada != '' && inputLlegada != '') {
-		$(location).attr('href','/search.html');
+		$('#buscar').click(buscarLugar);
+	}	
+}
+
+
+$(document).ready(function () {
+
+	var cargarPagina = function () {
+
+		$('.button-collapse').sideNav({
+			menuWidth: 380, 
+			edge: 'left', 
+			closeOnClick: false, 
+			draggable: true 
+		});
+		$('.modal').modal();
+		$('#inputs').change(validarInputs);
 	}
-}
 
-var cargarPagina = function () {
-
-	$('.button-collapse').sideNav({
-		menuWidth: 380, 
-		edge: 'left', 
-		closeOnClick: false, 
-		draggable: true 
-	});
-
-	$('.modal').modal();
-
-	$('#buscar').click(buscarLugar);
-}
-$(document).ready(cargarPagina);
+});
