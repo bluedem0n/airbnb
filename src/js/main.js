2336982fd3f9dc676133beb2532ed0853781bbdf
Andrea Maucaylli <andreamaucaylli@gmail.com>
2016-12-14 10:21:59 -0500

    local storage

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,34 @@
-var lugarIngresado = ;
-var llegadaIngresada = ;
-var salidaIngresada = ;
-var numHuespedes = ;
+var lugarIngresado = localStorage.getItem("");
+var llegadaIngresada = localStorage.getItem("");
+var salidaIngresada = localStorage.getItem("");
+var numHuespedes = localStorage.getItem("");
 
 var inputLugar = $('#lugar');
 var inputLlegada = $('#llegada');
 var inputSalida = $('#salida');
 var btnBuscar = $('#buscar');
 
-var validarTeclas = function () {
-	var ascii = 
-}
+var validarTeclas = function (e) {
+	var ascii = e.keyCode;
+		if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
+			return true;
+		} else {
+			return false;			
+		}
+	}
 
 var buscarLugar = function () {
+	var lugar = inputLugar.val();
+	lugarIngresado = localStorage.setItem("lugar", lugar);
+	
+	var llegada = inputSalida.val();
+	llegadaIngresada = localStorage.setItem("llegada", llegada);
+
+	var salida = inputLlegada.val();
+	salidaIngresada = localStorage.setItem("salida", salida);
+
 	if (inputLugar != '' && inputLlegada != '' && inputLlegada != '') {
-		btnBuscar.attr('href', 'localhost:3000/search.html');
+		$(location).attr('href','/search.html');
 	}
 }
 
